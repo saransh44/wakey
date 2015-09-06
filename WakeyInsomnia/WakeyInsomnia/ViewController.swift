50725d8a254f91f8d18f1aecdb1f8212c05e4d0a
Anand Tyagi <ananddtyagi@yahoo.com>
2015-09-05 23:39:49 -0400

    no message

---
+++ WakeyInsomnia/WakeyInsomnia/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  WakeyInsomnia
+//
+//  Created by Anand Tyagi on 9/5/15.
+//  Copyright (c) 2015 Anand Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let gradientLayer = CAGradientLayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        // 2
+        gradientLayer.frame = self.view.bounds
+        
+        // 3
+        let color1 = UIColor.redColor().CGColor as CGColorRef
+        let color2 = UIColor.orangeColor().CGColor as CGColorRef
+        let color3 = UIColor.yellowColor().CGColor as CGColorRef
+        
+        gradientLayer.colors = [color1, color2, color3]
+        
+        // 4
+        gradientLayer.locations = [0.0, 0.25, 0.75]
+        
+        // 5
+        //self.view.layer.addSublayer(gradientLayer)
+        //view.insertSublayer(gradientLayer, below: self.view.subviews[0].layer)
+        self.view.layer.insertSublayer(gradientLayer, below: self.view.subviews[0].layer)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+
+
+}
+
