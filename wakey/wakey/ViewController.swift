f389d23bced0cd2c68c0333e85db8e15df225f9d
Anand Tyagi <ananddtyagi@yahoo.com>
2015-09-05 22:06:23 -0400

    Fixed dat shit

---
+++ wakey/wakey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wakey
+//
+//  Created by Anand Tyagi on 9/5/15.
+//  Copyright (c) 2015 Anand Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
